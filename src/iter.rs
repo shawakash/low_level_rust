fe79f42a5f9c746191b4e7c7c60c6f2979c95484
Akash Shaw <shawakash2003@gmail.com>
2025-03-05 17:30:50 +0530

    chore: impl flatten of iter

---
+++ src/iter.rs
@@ -0,0 +1,96 @@
+pub trait IteratorExt: Iterator + Sized {
+    fn our_flatten(self) -> Flatten<Self>
+    where
+        Self::Item: IntoIterator;
+}
+impl<T> IteratorExt for T
+where
+    T: Iterator,
+{
+    fn our_flatten(self) -> Flatten<Self>
+    where
+        Self::Item: IntoIterator,
+    {
+        flatten(self)
+    }
+}
+
+pub fn flatten<I>(iter: I) -> Flatten<I::IntoIter>
+where
+    I: IntoIterator,
+    I::Item: IntoIterator,
+{
+    Flatten::new(iter.into_iter())
+}
+
+pub struct Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    outer: O,
+    front_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+    back_iter: Option<<O::Item as IntoIterator>::IntoIter>,
+}
+
+impl<O> Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    fn new(iter: O) -> Self {
+        Flatten {
+            outer: iter,
+            front_iter: None,
+            back_iter: None,
+        }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(front_iter) = &mut self.front_iter {
+                if let Some(i) = front_iter.next() {
+                    return Some(i);
+                }
+                self.front_iter = None;
+            }
+
+            if let Some(next_inner) = self.outer.next() {
+                self.front_iter = Some(next_inner.into_iter());
+            } else {
+                return self.back_iter.as_mut()?.next();
+            }
+        }
+    }
+}
+
+impl<O> DoubleEndedIterator for Flatten<O>
+where
+    O: DoubleEndedIterator,
+    O::Item: IntoIterator,
+    <O::Item as IntoIterator>::IntoIter: DoubleEndedIterator,
+{
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            if let Some(back_iter) = &mut self.back_iter {
+                if let Some(i) = back_iter.next_back() {
+                    return Some(i);
+                }
+                self.back_iter = None;
+            }
+
+            if let Some(next_back_inner) = self.outer.next_back() {
+                self.back_iter = Some(next_back_inner.into_iter());
+            } else {
+                return self.front_iter.as_mut()?.next_back();
+            }
+        }
+    }
+}
